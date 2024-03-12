3b1e081086ed2db100b19f226f9df8218861b97d
Lucas <lucasmc77700@gmail.com>
2024-03-12 12:33:29 +0100

    tile compatibility set-up

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Tile[] upNeighbours;
+    public Tile[] rightNeighbours;
+    public Tile[] downNeighbours;
+    public Tile[] leftNeighbours;
 }
